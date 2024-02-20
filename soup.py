4db0f4547e9b959c8b28a3ec73bb2cb95798de05
Trakuro <trakuro@buaa.edu.cn>
2024-02-21 06:05:01 +0800

    First commit of bsco

---
+++ soup.py
@@ -0,0 +1,11 @@
+import requests
+from bs4 import BeautifulSoup
+
+content = requests.get("https://wiki.biligame.com/reverse1999/%E9%AC%83%E6%AF%9B%E6%B2%99%E7%A0%BE").text
+
+soup = BeautifulSoup(content, "html.parser")
+
+# print(soup.prettify())
+
+
+
